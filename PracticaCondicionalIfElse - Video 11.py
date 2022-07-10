ea8f40cc4bba66e310d2d2f3326c54c1f34489ea
Milagros Salas <milagrossalas15@gmail.com>
2022-07-10 10:23:03 -0500

    Practicando python

---
+++ PracticaCondicionalIfElse - Video 11.py
@@ -0,0 +1,34 @@
+# EJERCICIO 1
+# num1 = int(input("Escribe un primer número: "))
+# num2 = int(input("Escribe un segundo número: "))
+#
+# def devuelveMax(n1, n2):
+#     if n1 > n2:
+#         print(n1)
+#     elif n2 > n1:
+#         print(n2)
+#     else:
+#         print("Son iguales")
+#
+# print("El número más alto es: " )
+# devuelveMax(num1, num2)
+
+
+# EJERCICIO 2
+# nombre = input("Escriba su Nombre: ")
+# direccion = input("Escriba su Dirección: ")
+# telefono = input("Escriba su Teléfono: ")
+#
+# miLista = [nombre, direccion, telefono]
+#
+# print(f"Los datos personales son: Sr(a). {nombre}, vive en {direccion} y su teléfono es {telefono}")
+
+
+# EJERCICIO 3
+num1 = int(input("Escribe un primer número: "))
+num2 = int(input("Escribe un segundo número: "))
+num3 = int(input("Escribe un tercer número: "))
+
+mediaAritmetica = (num1 + num2 + num3)/3
+
+print(f"La media aritmética de los números introducidos es: {mediaAritmetica}")
