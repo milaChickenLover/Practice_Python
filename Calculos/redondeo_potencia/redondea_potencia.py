effea222f82253086faf36281de4875285a9c6de
MilaChromebook <milagrossalas15@gmail.com>
2022-08-13 18:46:14 -0500

    ultimos cambios

---
+++ Calculos/redondeo_potencia/redondea_potencia.py
@@ -0,0 +1,5 @@
+def potencia(base,exponente):
+    print(f"El resultado del cálculo es: {base ** exponente}")
+
+def redondear(numero):
+    print(f"El resultado del cálculo es: {round(numero)}")
--- uso_modulos.py
+++ uso_modulos.py
@@ -1,3 +1,5 @@
-from Calculos.calculos_generales import *
+from Calculos.basicos.operaciones_basicas import *
+from Calculos.redondeo_potencia.redondea_potencia import *
 
 dividir(4,6)
+potencia(2,3)
